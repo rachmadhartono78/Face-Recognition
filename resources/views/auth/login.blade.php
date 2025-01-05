76241f5573e61c046c3d55e9189c92fb0528613f
rachmadhartono78 <rahmadhartono78@gmail.com>
2025-01-05 12:27:20 +0700

    redeploy

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
             <a href="index.html"><img src="{{ asset('/images/logo/logo.png') }}" alt="Logo"></a>
         </div>
         <h1 class="auth-title">Log in.</h1>
-        <p class="auth-subtitle mb-5">Silakan login terlebih dahulu.</p>
+        <p class="auth-subtitle mb-5">your data will be safe.</p>
 
         @if (session('status'))
         <div class="mb-4 font-medium text-sm text-green-600">
