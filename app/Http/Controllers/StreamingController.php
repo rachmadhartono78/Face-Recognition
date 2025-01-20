91d7b8e066e9d6ee45a58f2b9736efd9ca624556
rachmadhartono78 <rahmadhartono78@gmail.com>
2025-01-20 11:01:59 +0700

    bismillah

--- app/Http/Controllers/StreamingController.php
+++ app/Http/Controllers/StreamingController.php
@@ -9,9 +9,21 @@ class StreamingController extends Controller
 public function startStreaming()
 {
 
-    $flaskUrl = "http://127.0.0.1:5000/stream";
-    return view('admin.livemonitoring.streaming', compact('flaskUrl'));
+    // $flaskUrl = "http://127.0.0.1:5000/stream";
+    // return view('admin.livemonitoring.streaming', compact('flaskUrl'));
 
+        // URL pertama (lokal)
+        $flaskUrl = "http://127.0.0.1:5000/stream";
 
-}
+        // Cek apakah URL pertama dapat diakses
+        $response = Http::get($flaskUrl);
+
+        // Jika URL pertama tidak dapat diakses, ganti dengan URL kedua
+        if (!$response->successful()) {
+            $flaskUrl = "http://103.220.113.186:8080/";
+        }
+
+        // Kirim URL yang telah dipilih ke view
+        return view('admin.livemonitoring.streaming', compact('flaskUrl'));
+    }
 }
